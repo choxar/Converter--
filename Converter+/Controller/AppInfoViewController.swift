df6f849755eb14515e7f7fcba49ab0af0796e3f4
choxar <elinazambere@yahoo.com>
2021-02-15 16:57:01 +0200

    Done Project

---
+++ Converter+/Controller/AppInfoViewController.swift
@@ -0,0 +1,36 @@
+//
+//  AppInfoViewController.swift
+//  Converter+
+//
+//  Created by elina.zambere on 10/02/2021.
+//
+
+import UIKit
+
+class AppInfoViewController: UIViewController {
+    
+    var infoText = String()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func closeButtonTapped(_ sender: Any) {
+        
+        self.dismiss(animated: true, completion: nil)
+        
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
